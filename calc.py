1e4af85a1359bd6d24f8ccfe03e42d545c607303
Mark Ruddy <1markruddy@gmail.com>
2022-11-20 09:17:00 +0000

    add initial

---
+++ calc.py
@@ -0,0 +1,14 @@
+# second_grade = 84
+#second_grade = round((84 + 92 + 70 + 91 + 86 + 82 + 76) / 7, 2)
+second_grade = round((84 + 92 + 70 + 91 + 86 + (82 + 76 / 200)) / 6, 2)
+final_grade = 65
+
+sec = round(second_grade * 0.3, 2)
+fin = round(final_grade * 0.7, 2)
+overall = round(sec + fin, 2)
+
+print(f"Second year overall: {second_grade}")
+print(f"Final year overall: {final_grade}")
+print(f"Second weighted: {sec}")
+print(f"Final weighted: {fin}")
+print(f"Overall: {overall}")
